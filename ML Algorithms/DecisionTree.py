211afb0a16996b9c616967b82c7bee779a7c8f5c
Srivaishnavi Yaddanapudi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2024-10-27 22:48:58 +0530

    Clean code, use standard conventions for `train_test_score`, and update comments

--- ML Algorithms/DecisionTree.py
+++ ML Algorithms/DecisionTree.py
@@ -1,34 +1,40 @@
-#Decision Tree algorithm for by using dataset of mobile retail shop to predict if the user buys the mobile or not
-
+from re import X
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 %matplotlib inline
 import warnings
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
 
 warnings.filterwarnings("ignore")
 mobile_data = pd.read_csv("mobiledata.csv")
 
-#to select features or attributes by neglecting purchases_mobile feature
-features = mobile_data.columns.drop("purchases_mobile")
-
 # the target that the model need to predict
 target = ["purchases_mobile"]
 
-mobile_data_encoded = pd.get_dummies(mobile_data[features])
+# encode data using one-hot encoding
+mobile_data_encoded = pd.get_dummies(mobile_data.drop(target, axis=1)).astype(int)
+
 print(f"mobile data encoded is: {mobile_data_encoded}")
-mobile_data_encoded["purchases_mobile"] = mobile_data["purchases_mobile"]
+mobile_target = mobile_data[target]
 
-from sklearn.model_selection import train_test_split
-mobile_train,mobile_test = train_test_split(mobile_data_encoded,test_size=0.15,random_state=100)
+# features excluding target
+features = mobile_data_encoded.columns
 
-from sklearn.tree import DecisionTreeClassifier
+# split data into training and testing sets
+# test split is 15% of original data
+X_train, X_test, y_train, y_test = train_test_split(mobile_data_encoded, mobile_target, test_size=0.15, random_state=100)
+
+# train model
 model = DecisionTreeClassifier()
-target = ["purchases_mobile"]
-features = mobile_train.columns.drop("purchases_mobile")
-model.fit(mobile_train[features],mobile_train[target])
+model.fit(X_train, y_train)
 
-from sklearn.tree import plot_tree
+# evaluate model
+print(model.score(X_test, y_test))
+
+# visualize tree
 plt.figure(figsize=(20,10))
 plot_tree(model,
           feature_names=features,
